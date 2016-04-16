a982c8caac52a9540c280a855a1cd840640dc530
dionescu <dionescu88@gmail.com>
2016-04-16 19:40:01 -0400

    add msan example

---
+++ examples/msan/msan.cc
@@ -0,0 +1,12 @@
+// This file demonstrates the usage of the MSAN sanitizer.
+//
+// bazel run --config=msan :msan
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+  int* a = new int[10];
+  a[5] = 0;
+  if (a[argc])
+    printf("xx\n");
+  return 0;
+}
